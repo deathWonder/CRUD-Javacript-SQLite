f79429f7eb72ce84e23e6f95d2160c80624d5be2
Сабир <bovba179@gmail.com>
2024-06-01 21:34:58 +0300

    Initial work

---
+++ routes/userRoutes/updateUser.js
@@ -0,0 +1,29 @@
+const data = require('../../sql3-data');
+
+module.exports = (req, res) => {
+    const id = parseInt(req.url.split('/')[2]);
+    let body = '';
+
+    req.on('data', chunk => {
+        body += chunk;
+    });
+
+    req.on('end', async () => {
+        const parsedBody = new URLSearchParams(body);
+        const updateData = {};
+
+        parsedBody.forEach((value, key) => {
+            updateData[key] = key === 'age' ? parseInt(value) : value;
+        });
+
+        const updatedUser = await data.updateUser(id, updateData);
+
+        if(updatedUser) {
+            res.writeHead(200);
+            res.end(JSON.stringify(updatedUser));
+        } else {
+            res.writeHead(404);
+            res.end(JSON.stringify({ message: 'User not found'}));
+        }
+    });
+};
